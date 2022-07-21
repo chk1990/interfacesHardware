a6090f50add91a8aad0bc12eb6eb0c7bed1cded5
Christoph Kolhoff <93308857+chk1990@users.noreply.github.com>
2022-07-21 13:34:34 +0200

    Added file for group names

---
+++ group_names_doxy.h
@@ -0,0 +1,11 @@
+#ifndef _GROUP_NAMES_H
+#define _GROUP_NAMES_H
+
+/** @defgroup devices Devices
+ *  Devices to be interfaced using the classes of the module \ref linuxInterfaces.
+ *  This group contains several functions for interfacing independent devices.
+ *  @{
+ */
+
+/** @} */ // end of group "devices"
+#endif // _GROUP_NAMES_H
